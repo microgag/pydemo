2c694f048fca1153ed96be59c8d31852fed9d5ee
microgag <16010631+microgag@users.noreply.github.com>
2022-10-26 19:44:52 +0800

    Create lonelyinteger.py
    Given an array of integers, where all elements but one occur twice, find the unique element.
    
    Example
    
    The unique element is .
    
    Function Description
    
    Complete the lonelyinteger function in the editor below.
    
    lonelyinteger has the following parameter(s):
    
    int a[n]: an array of integers
    Returns
    
    int: the element that occurs only once
    Input Format
    
    The first line contains a single integer, , the number of integers in the array.
    The second line contains  space-separated integers that describe the values in .
    
    Constraints
    
    It is guaranteed that  is an odd number and that there is one unique element.
    , where .

---
+++ lonelyinteger.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def lonelyinteger(a):
+    result = [val for val in a if a.count(val) == 1]
+    for i in result:
+        return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
